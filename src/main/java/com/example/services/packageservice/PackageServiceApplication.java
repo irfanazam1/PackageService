74170505b75a113f9c4c1d049b8fbdb89333d308
irfan.azam <irfanazam1@outlook.com>
2022-07-19 12:19:30 +0500

    Checking a new image version

--- src/main/java/com/example/services/packageservice/PackageServiceApplication.java
+++ src/main/java/com/example/services/packageservice/PackageServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+// just checking a new image version
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PackageServiceApplication {
     public static void main(String[] args) {
